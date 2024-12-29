4647998271e49aa3f4e0926d5ee988516c006e6e
ACZRO <aczrproject@gmail.com>
2024-12-28 23:32:38 -0500

    v02

---
+++ test_emotion_detection.py
@@ -0,0 +1,27 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):
+        # Test case for joy
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+        # Test case for anger
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result["dominant_emotion"], "anger")
+
+        # Test case for disgust
+        result = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result["dominant_emotion"], "disgust")
+
+        # Test case for sadness
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result["dominant_emotion"], "sadness")
+
+        # Test case for fear
+        result = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result["dominant_emotion"], "fear")
+
+if __name__ == "__main__":
+    unittest.main()
